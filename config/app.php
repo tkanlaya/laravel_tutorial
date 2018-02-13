53f74039121fbb6f95360164151013fa3428258b
thanatcha <thanatchapron@haii.or.th>
2018-02-13 18:03:06 +0700

    css(bootstarp)+js

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -175,6 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'HTML'=> Illuminate\html\HtmlFacades,
 
     ],
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,9 @@
         <meta charset="utf-8">
         <title>@yield('title')</title>
 
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css') }}">
+       <!--  <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css') }}"> -->
+     {{ Html::style(('css/bootstrap.css')) }}
+
     </head>
     <body>
         <h1>Layout Main</h1>
@@ -12,7 +14,11 @@
             @yield('content')
         </div>
 
-        <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+       <!--  <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script -->>
+
+            {{ Html::script('js/jquery.min.js') }}
+            {{ Html::script('js/bootstrap.min.js') }}
+
     </body>
 </html>
